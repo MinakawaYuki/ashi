20663792575546810ef5f1486c4242ca1d528028
MinakawaYuki <tianyu.zhang@tarsocial.com>
2022-07-26 11:54:59 +0800

    上传

--- router/api.go
+++ router/api.go
@@ -4,10 +4,13 @@ import (
 	"ashi/controller"
 	"ashi/middleware"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Register(r *gin.Engine) {
+	r.StaticFS("/upload", http.Dir("./runtime/upload"))
 	user(r)
+	upload(r)
 }
 
 /**
@@ -18,7 +21,20 @@ func user(r *gin.Engine) {
 	group.Use(middleware.JWTAuth())
 	{
 		c := &controller.UserController{}
-		group.POST("/add", c.Add)
-		group.POST("/login", c.Login)
+		group.POST("/add", c.Add)     //注册
+		group.POST("/login", c.Login) //登录
+	}
+}
+
+/**
+上传
+*/
+func upload(r *gin.Engine) {
+	var group = r.Group("/api/upload")
+	group.Use(middleware.JWTAuth())
+	{
+		c := &controller.UploadController{}
+		group.POST("/file", c.UploadFile)   //单文件上传
+		group.POST("/files", c.UploadFiles) //多文件上传
 	}
 }
---
+++ utils/upload.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+	"path"
+	"time"
+)
+
+const fileRouter = "/upload/"
+
+func init() {
+	_, err := os.Stat("./runtime/upload")
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll("./runtime/upload", 0666)
+		}
+	}
+}
+func UploadFile(c *gin.Context) (filePath string, err error) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return "", err
+	}
+	extName := path.Ext(file.Filename)
+	filename := getFileName(file.Filename) + extName
+	dst := path.Join("./runtime/upload", filename)
+	saveErr := c.SaveUploadedFile(file, dst)
+	if saveErr != nil {
+		return "", saveErr
+	}
+	return fileRouter + filename, nil
+}
+
+func UploadFiles(c *gin.Context) (filePath []string, err error) {
+	var urls []string
+	form, _ := c.MultipartForm()
+	files := form.File["files"]
+	for _, file := range files {
+		extName := path.Ext(file.Filename)
+		filename := getFileName(file.Filename) + extName
+		dst := path.Join("./runtime/upload", filename)
+		urls = append(urls, fileRouter+filename)
+		err := c.SaveUploadedFile(file, dst)
+		if err != nil {
+			return make([]string, 0), err
+		}
+	}
+	return urls, nil
+}
+
+func getFileName(filename string) string {
+	//3、创建图片保存目录,linux下需要设置权限（0666可读可写） static/upload/20200623
+	day := time.Now().Format("20060102")
+	name := Md5(day + filename)
+	return name
+}
